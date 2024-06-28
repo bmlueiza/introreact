b201c39c55defd9d9daa8b3aae635ab838563ca5
Belén <belen.munoz@usach.cl>
2024-06-27 22:16:51 -0400

    chore: pasarle atributos a un componente
    - Al componente Header dentro de App.jsx

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.css";
 
-function Header() {
+// Componentes
+// Componentes son funciones que retornan pseudo html
+// Se pueden pasar propiedades a los componentes
+
+// Componente Header
+// En JSX, los props se acceden a traves de un objeto que se pasa como primer argumento de la funcion (props por convencion)
+// También podría colocarse {home, nosotros, contacto} en lugar de props y acceder a las propiedades directamente
+function Header(props) {
   return (
     <header>
       <nav>
-        <h1>hola mundo</h1>
+        <ul>
+          <li>
+            <a href="#">{props.home}</a>
+          </li>
+          <li>
+            <a href="#">{props.nosotros}</a>
+          </li>
+          <li>
+            <a href="#">{props.contacto}</a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
@@ -25,16 +42,22 @@ function Footer() {
 }
 
 function App() {
+  {
+    console.log(
+      "este es un render donde se muestra el header, main y footer. Se retorna pseudo html con jsx"
+    );
+  }
+  {
+    console.log(
+      "No se puede retornar dos elementos html sin un contenedor padre, por eso se usa el fragment <></>"
+    );
+  }
+  {
+    console.log("para escribir codigo js se usa {} ");
+  }
   return (
     <>
-      {console.log(
-        "este es un render donde se muestra el header, main y footer. Se retorna pseudo html con jsx"
-      )}
-      {console.log(
-        "No se puede retornar dos elementos html sin un contenedor padre, por eso se usa el fragment <></>"
-      )}
-      {console.log("para escribir codigo js se usa {} ")}
-      <Header />
+      <Header nosotros="nosotros" home="home" contacto="contacto" />
       <Main />
       <Footer />
     </>
